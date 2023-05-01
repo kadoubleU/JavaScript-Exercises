cf5613a2f23525838099e0a052fdf4029b59b1cb
kadoubleU <delievabg@gmail.com>
2023-05-01 15:40:24 +0300

    Upload new files

---
+++ 00. Projects and Excercises/01.Excercises/01.fundamentals-part01/assigments.js
@@ -0,0 +1,16 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+// Excercise 1: Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+
+let country = "Bulgaria";
+let continent = "Europe";
+let population = "6.8";
+
+// Excercise 2: Log their values to the console
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 00. Projects and Excercises/01.Excercises/01.fundamentals-part01/script.js
@@ -0,0 +1,4 @@
+
+      let js = 'amazing';
+      
+      console.log(js);
