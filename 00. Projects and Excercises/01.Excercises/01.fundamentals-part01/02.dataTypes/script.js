ce58b3f37c9050a0df63797496361fde9cfa1db7
kadoubleU <delievabg@gmail.com>
2023-05-02 16:05:31 +0300

    Upload new files

---
+++ 00. Projects and Excercises/01.Excercises/01.fundamentals-part01/02.dataTypes/script.js
@@ -0,0 +1,20 @@
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
+// Excercise 1: Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language', but don't assign it any value yet
+
+let isIsland = false;
+let language;
+
+//From Previous Excercise 
+
+let country = "Bulgaria";
+let continent = "Europe";
+let population = "6.8";
+
+// Excercise 2: Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
---
+++ 00. Projects and Excercises/01.Excercises/01.fundamentals-part01/03.let-const-var/script.js
@@ -0,0 +1,50 @@
+// LECTURE: Data Types
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
+//------------------------------------------------------------
+
+// Excercise 1: Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language', but don't assign it any value yet
+
+// let isIsland = false;
+// let language;
+
+//From Previous Excercise 
+
+// let country = "Bulgaria";
+// let continent = "Europe";
+// let population = "6.8";
+
+//-----------------------------------------------------------
+
+// Excercise 2: Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof language);
+
+
+// ------------------------------------------------------
+//-------------------------------------------------------
+
+// LECTURE: let, const and var
+// 1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
+// 2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+// 3. Try to change one of the changed variables now, and observe what happens
+
+
+
+// Excercise 1: Set the value of 'language' to the language spoken where you live.
+
+language = "Bulgarian";
+
+// Excercise 2: Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+
+const isIsland = false;
+const country = "Bulgaria";
+const continent = "Europe";
+
+// 3. Try to change one of the changed variables now, and observe what happens
+
+country = "England";
